825dbe6ee9a9c4e22e9ff0da695a5a85355e7fb7
andreyirk <sokolik_85@list.ru>
2020-04-26 13:53:26 +0300

    point refactor

---
+++ src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,33 @@
+package ru.job4j.condition;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+public class Point {
+
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point point2) {
+        return sqrt(pow(point2.x - this.x, 2) + pow(point2.y - this.y, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
+    }
+}
