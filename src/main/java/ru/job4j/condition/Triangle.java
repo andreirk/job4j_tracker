d287641c65e03862aeb9163a79267df18e5397ee
andreyirk <sokolik_85@list.ru>
2020-04-26 13:59:56 +0300

    traingle

---
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,67 @@
+package ru.job4j.condition;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    /**
+     * Метод вычисления периметра по длинам сторон.
+     *
+     * Формула.
+     *
+     * (a + b + c) / 2
+     *
+     * @param a расстояние между точками a b
+     * @param b расстояние между точками a c
+     * @param c расстояние между точками b c
+     * @return Периметр.
+     */
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2 ;
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     *
+     * Формула.
+     *
+     * √ p *(p - ab) * (p - ac) * (p - bc)
+     *
+     * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
+     *
+     * @return Вернуть площадь, если треугольник существует или -1.
+     */
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            // написать формулу для расчета площади треугольника.
+            rsl = p *(p - a * b) * (p - a * c) * (p - b * c);
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     *
+     * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
+     *
+     * @param a Длина от точки a b.
+     * @param b Длина от точки a c.
+     * @param c Длина от точки b c.
+     * @return
+     */
+    private boolean exist(double a, double c, double b) {
+        return a > 0 && b > 0 && c > 0;
+    }
+}
