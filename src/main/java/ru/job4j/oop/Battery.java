0bb2877517ff0b89244ccf8fb86389d7e431ca4c
andreyirk <sokolik_85@list.ru>
2020-04-26 13:48:10 +0300

    колобок

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int power;
+    public Battery(int power){
+        this.power = power;
+    }
+
+    public void exchange(Battery another){
+        another.power += this.power;
+        this.power = 0;
+    }
+}
