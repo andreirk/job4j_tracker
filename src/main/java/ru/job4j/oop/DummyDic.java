f7602c2d643b95aae8f693a8650d7ab205102bdf
andreyirk <sokolik_85@list.ru>
2020-04-26 12:38:30 +0300

    dummi dict

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+    public String engToRus(String eng){
+        return new StringBuilder(14)
+                .append("Нейзвестное слово")
+                .append(": ")
+                .append(eng)
+                .toString();
+    }
+
+    public static void main(String[] args) {
+        DummyDic dict = new DummyDic();
+        System.out.println(dict.engToRus("capital"));
+    }
+}
