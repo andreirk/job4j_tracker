3ea01b8343b3afd83ba77b28dae864d885a6260e
andreyirk <sokolik_85@list.ru>
2020-04-27 08:48:37 +0300

    books

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,18 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book[] books = new Book[3];
+        for(int i = 0; i < 3; i++){
+            Book book = new Book();
+            book.setName("Clean Code" + i);
+            books[i] = book;
+        }
+
+        for (Book book: books
+             ) {
+            System.out.println("The book is: "+ book.getName());
+        }
+    }
+}
