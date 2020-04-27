a0f5582aa470dffd8d438c96571a54488f37b9c9
andreyirk <sokolik_85@list.ru>
2020-04-27 10:23:27 +0300

    Tracker and Item

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -12,7 +15,8 @@ public class Tracker {
     }
 
     private String generateId() {
-        return String.valueOf(ids++);
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
     }
 
     public Item findById(String id) {
@@ -25,5 +29,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+    public Item findByName(String name) {
+        Item rsl = null;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(name)) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+
+
+    public Item[] findAll(){
+        int size = 0;
+        Item[] result = new Item[this.items.length];
+        for(int i = 0; i < result.length; i++){
+            if(this.items[i] != null){
+                result[size] = this.items[i];
+                size++;
+            }
+        }
+
+        return Arrays.copyOf(result, size);
     }
 }
