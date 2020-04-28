fc788a3c30d8fac99b7ca0e1f066970ebea3b914
andreyirk <sokolik_85@list.ru>
2020-04-28 12:53:04 +0300

    transport interface

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passangers(int num) {
+
+    }
+
+    @Override
+    public int fuel(int qnt) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void drive();
+    void passangers(int num);
+    int fuel(int qnt);
+}
