ae1ea1f2143d28ca0efa019f5b3a92d007b293f3
andreyirk <sokolik_85@list.ru>
2020-05-05 08:48:34 +0300

    exeptions user

---
+++ src/main/java/ru/job4j/ex/UserInvalidException.java
@@ -0,0 +1,7 @@
+package ru.job4j.ex;
+
+public class UserInvalidException extends UserNotFoundException {
+    UserInvalidException(String msg){
+        super(msg);
+    }
+}
---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,40 @@
+package ru.job4j.ex;
+
+import java.util.Arrays;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User user : users) {
+            if(user.getUsername().equals(login)){
+                return user;
+            }
+        }
+
+        throw new UserNotFoundException("User not found");
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if(!user.isValid() || user.getUsername().length() < 3){
+            throw new UserInvalidException("Not valid user");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ex){
+            System.out.println("User is not valid");
+        } catch (UserNotFoundException ex){
+            System.out.println("User not found");
+        }
+
+    }
+}
