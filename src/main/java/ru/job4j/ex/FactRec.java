c3ece00f4a81d518e3a74d7134563454ebc349da
andreyirk <sokolik_85@list.ru>
2020-05-05 09:09:42 +0300

    exeptions factorial

---
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -0,0 +1,13 @@
+package ru.job4j.ex;
+
+public class FactRec {
+    public static long calc(int n) {
+        if(n <= 1) return 1;
+        return calc(n - 1) * n;
+    }
+
+    public static void main(String[] args) {
+        long rsl = calc(10);
+        System.out.println(rsl);
+    }
+}
