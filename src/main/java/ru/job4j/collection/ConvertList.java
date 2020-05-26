84370dc441fbff4978bd89a20adcb855294dd80e
andreyirk <sokolik_85@list.ru>
2020-05-26 20:25:25 +0300

    ConvertList

---
+++ src/main/java/ru/job4j/collection/ConvertList.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertList {
+    public static List<Integer> convert(List<int[]> list) {
+        List<Integer> rsl = new ArrayList<>();
+
+        for (int[] arrOfNumbers : list) {
+            for (int number : arrOfNumbers) {
+                rsl.add(number);
+            }
+        }
+
+        /// for-each
+        return rsl;
+    }
+}
