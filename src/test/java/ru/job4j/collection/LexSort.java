7f8800d8065b1c2c26323303820c2e8955439703
andreyirk <sokolik_85@list.ru>
2020-05-28 12:23:50 +0300

    sort string numbers

---
+++ src/test/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+
+
+        String[] leftArr = left.split("");
+        String[] rightArr = right.split("");
+
+        String leftStringToParse = "";
+        int indexInLeft = 0;
+        while (!leftArr[indexInLeft].equals(".")){
+            leftStringToParse += leftArr[indexInLeft];
+            indexInLeft++;
+        }
+        String rightStringToParse = "";
+        int indexInRight = 0;
+        while (!rightArr[indexInRight].equals(".")){
+            rightStringToParse += rightArr[indexInRight];
+            indexInRight++;
+        }
+
+
+        int intRight = Integer.parseInt(rightStringToParse);
+        int intLeft = Integer.parseInt(leftStringToParse);
+        return  Integer.compare(intLeft, intRight);
+    }
+}
