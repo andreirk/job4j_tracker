621ec2a1406ef65b8eb23dd8a7595fdaf2edd916
andreyirk <sokolik_85@list.ru>
2020-06-21 10:15:54 +0300

    filter students with Stream API

---
+++ src/main/java/ru/job4j/lambda/School.java
@@ -0,0 +1,14 @@
+package ru.job4j.lambda;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+
+    public  static List<Student> collect(List<Student> students, Predicate<Student> predicate) {
+        return students.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/ru/job4j/lambda/SchoolTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.lambda;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class SchoolTest {
+    List<Student> students;
+
+    @Before
+    public void initialize() {
+        students = List.of(
+                new Student("Petrov", 10),
+                new Student("Sidoroov", 20),
+                new Student("Ivanov", 30),
+                new Student("Chizov", 50),
+                new Student("Chizikov", 96),
+                new Student("Kozlov", 60),
+                new Student("Bobrov", 80)
+                );
+    }
+
+    @Test
+    public void whenFilterToClassA() {
+        List<Student> result = School.collect(students, (s) -> s.getScore() > 70);
+        result.stream().map(s -> s.getSurname()).forEach(System.out::println);
+        assertThat(result, containsInAnyOrder(
+                new Student("Bobrov", 80),
+                new Student("Chizikov", 96)
+        ));
+    }
+
+    @Test
+    public void whenFilterToClassB() {
+        List<Student> result = School.collect(students, (s) -> s.getScore() >= 50 && s.getScore() < 70);
+        result.stream().map(s -> s.getSurname()).forEach(System.out::println);
+        assertThat(result, containsInAnyOrder(
+                new Student("Kozlov", 60),
+                new Student("Chizov", 50)
+        ));
+    }
+
+    @Test
+    public void whenFilterToClassC() {
+        List<Student> result = School.collect(students, (s) -> s.getScore() < 50 );
+        result.stream().map(s -> s.getSurname()).forEach(System.out::println);
+        assertThat(result, containsInAnyOrder(
+                new Student("Petrov", 10),
+                new Student("Sidoroov", 20),
+                new Student("Ivanov", 30)
+        ));
+    }
+
+}
